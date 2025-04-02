c6724ed02d8649455ded3d6df49371ae26685b00
CsarNarciso <cesarpazol1029@gmail.com>
2025-04-01 19:37:09 -0600

    Make my projects title a button element instead an a hyperlink one, grouping text and image on the side, centering both elements and adding js click event for github profile redirection

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	
+	document.getElementById('projectsListTitleBtn').onclick = function() {
+		
+		//Redirect to Github profile
+		window.location.href = "https://github.com/CsarNarciso";	
+	};
+};
