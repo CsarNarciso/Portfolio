fed1386f0d5e2afff841fe78dd209c95d4658e0a
CsarNarciso <cesarpazol1029@gmail.com>
2025-04-07 07:04:17 -0600

    Add contact me section and icons using Awesome Fonts API, add color to role presentation text

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload = function() {
 	
-	document.getElementById('projectsListTitleBtn').onclick = function() {
+	document.getElementById('emailIcon').onclick = function() {
+		
+		//Redirect to 'Mail to'
+		window.location.href = "mailto:pozolcnarciso@gmail.com";	
+	};
+	
+	document.getElementById('linkedinIcon').onclick = function() {
+		
+		//Redirect to LinkedIn profile
+		window.location.href = "https://www.linkedin.com/in/cesar-pozol-narciso-b48727180/";	
+	};
+	
+	document.getElementById('githubIcon').onclick = function() {
 		
 		//Redirect to Github profile
 		window.location.href = "https://github.com/CsarNarciso";	
