0c953c1ab02315fde3e65809831bcf4690a1cfb5
CsarNarciso <cesarpazol1029@gmail.com>
2025-04-10 20:55:41 -0600

    Add availability tags for each project with redirection action and opacity for no available ones. Change role presentation text as DriverLife software creator

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 	
+	
+	//Contact Me section redirections
+	
 	document.getElementById('emailIcon').onclick = function() {
 		
 		//Redirect to 'Mail to'
@@ -17,4 +20,19 @@ window.onload = function() {
 		//Redirect to Github profile
 		window.location.href = "https://github.com/CsarNarciso";	
 	};
+	
+	
+	//Availability project tags redirections
+	
+	document.getElementById('chatWebProjectAvailabilityTags').querySelector('.sourceCode__icon ').onclick = function() {
+		window.location.href = "https://github.com/CsarNarciso/Chat-Web";	
+	}
+	
+	document.getElementById('driverLifeProjectAvailabilityTags').querySelector('.website__icon ').onclick = function() {
+		window.location.href = "https://trackaholics.forumotion.com/t9278-i-want-to-code-an-app-for-gtr2-how-to-access-game-telemetry-data";	
+	}
+	
+	document.getElementById('driverLifeProjectAvailabilityTags').querySelector('.download__icon ').onclick = function() {
+		window.location.href = "https://www.mediafire.com/file/rj86k0f3m2uumye/DriverLife.zip/file";	
+	}
 };
