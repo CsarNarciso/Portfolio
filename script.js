bdb117cec5338d88cc904b9f312159c40b0b3f51
CsarNarciso <cesarpazol1029@gmail.com>
2025-04-12 17:26:17 -0600

    Reduce driverlife video presentation duration to reduce host size, remove driverlife download availability tag and redirection to mediadfire

--- script.js
+++ script.js
@@ -28,11 +28,8 @@ window.onload = function() {
 		window.location.href = "https://github.com/CsarNarciso/Chat-Web";	
 	}
 	
+	
 	document.getElementById('driverLifeProjectAvailabilityTags').querySelector('.website__icon ').onclick = function() {
 		window.location.href = "https://trackaholics.forumotion.com/t9278-i-want-to-code-an-app-for-gtr2-how-to-access-game-telemetry-data";	
-	}
-	
-	document.getElementById('driverLifeProjectAvailabilityTags').querySelector('.download__icon ').onclick = function() {
-		window.location.href = "https://www.mediafire.com/file/rj86k0f3m2uumye/DriverLife.zip/file";	
 	}
 };
